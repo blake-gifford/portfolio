0fc8addbbef5825d3c441bc076893e9fea02c2ca
blake-gifford <blakegifford01@gmail.com>
2022-03-15 16:51:41 -0700

    added components and layouts for UI

---
+++ components/Common/AboutMe.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const AboutMe = () => {
+    return(
+        <div id='intro' className='intro-background-blue'>
+            <div className='intro-text'>
+                <h2>Blake Gifford</h2>
+                <h2>Web and iOS Developer.</h2>
+            </div>
+        </div>
+    )
+};
+
+export default AboutMe;
