6fd6e7eff5c11203b5a6af3d40dada2c59cea71e
blake-gifford <blakegifford01@gmail.com>
2022-03-15 16:58:28 -0700

    updated class error

--- components/Common/AboutMe.js
+++ components/Common/AboutMe.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const AboutMe = () => {
     return(
-        <div id='intro' className='intro-background-blue'>
+        <div id='intro'>
             <div className='intro-text'>
                 <h2>Blake Gifford</h2>
                 <h2>Web and iOS Developer.</h2>
