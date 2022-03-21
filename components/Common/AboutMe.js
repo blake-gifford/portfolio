ed22f5d7835a837bbaacdffee3bcb97e78226341
blake-gifford <blakegifford01@gmail.com>
2022-03-21 13:28:19 -0700

    added social links and updated components

--- components/Common/AboutMe.js
+++ components/Common/AboutMe.js
@@ -7,7 +7,7 @@ const AboutMe = () => {
             <div className="intro-text text">
                 <h2>Blake Gifford</h2>
                 <h2>Full Stack Web and iOS Developer</h2>
-                <h3 className="a-tag-style"> <a href='https://www.linkedin.com/in/blake-gifford/' target='_blank' ><AiFillLinkedin /></a> <a href='https://github.com/blake-gifford' target='_blank' >< AiFillGithub /></a> <a href='https://twitter.com/blakegifford_' target='_blank' ><AiFillTwitterCircle /></a> </h3>
+                <h3 className="a-tag-style"> <a href='https://www.linkedin.com/in/blake-gifford/' rel="noreferrer" target='_blank' ><AiFillLinkedin /></a> <a href='https://github.com/blake-gifford' rel="noreferrer" target='_blank' >< AiFillGithub /></a> <a href='https://twitter.com/blakegifford_' rel="noreferrer" target='_blank' ><AiFillTwitterCircle /></a> </h3>
                 
             </div>
         </div>
