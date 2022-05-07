499ae6cb98bdf2b7ecfaca48f7c4f2a42a35c1cf
blake-gifford <blakegifford01@gmail.com>
2022-05-06 18:14:23 -0700

    added recipe project

--- components/Common/PersonalProjects.js
+++ components/Common/PersonalProjects.js
@@ -6,7 +6,25 @@ const PersonalProjects = () => {
         <div id="personal" className="container-height title-style">
             <h2>Personal Projects</h2>
             <div className=" card-style card-div-spacing">
-                <div className="card card-height" style={{"width": "18rem"}}>
+                <div className="card card-height" style={{ "width": "18rem" }}>
+                    <Image height={325} width={500} className="card-img-top" src="/images/recipe-website.png" alt="Card image cap" />
+                    <div className="card-body">
+                        <h5 className="card-title">Trip App</h5>
+                        <p className="card-text">A MERN app built for users to plan and organize future events/trips they want to go on.</p>
+                        <a href="https://github.com/blake-gifford/Trip_App" target='_blank' className="btn btn-primary card-btn-style">Github</a>
+                        <a href="https://recipe-book.blakegifford.com/" target='_blank' className="btn btn-primary card-btn-style">Demo</a>
+                    </div>
+                </div>
+                {/* <div className="card card-height" style={{ "width": "18rem" }}>
+                    <Image height={325} width={500} className="card-img-top" src="/images/test.jpg" alt="Card image cap" />
+                    <div className="card-body">
+                        <h5 className="card-title">Card title</h5>
+                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card"s content.</p>
+                        <a href="#" className="btn btn-primary card-btn-style">Github</a>
+                        <a href="#" className="btn btn-primary card-btn-style">Demo</a>
+                    </div>
+                </div> */}
+                {/* <div className="card card-height" style={{"width": "18rem"}}>
                 <Image height={325} width={500} className="card-img-top" src="/images/test.jpg" alt="Card image cap" />
                 <div className="card-body">
                     <h5 className="card-title">Card title</h5>
@@ -14,25 +32,7 @@ const PersonalProjects = () => {
                     <a href="#" className="btn btn-primary card-btn-style">Github</a>
                     <a href="#" className="btn btn-primary card-btn-style">Demo</a>
                 </div>
-                </div>
-                <div className="card card-height" style={{"width": "18rem"}}>
-                <Image height={325} width={500} className="card-img-top" src="/images/test.jpg" alt="Card image cap" />
-                <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card"s content.</p>
-                    <a href="#" className="btn btn-primary card-btn-style">Github</a>
-                    <a href="#" className="btn btn-primary card-btn-style">Demo</a>
-                </div>
-                </div>
-                <div className="card card-height" style={{"width": "18rem"}}>
-                <Image height={325} width={500} className="card-img-top" src="/images/test.jpg" alt="Card image cap" />
-                <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card"s content.</p>
-                    <a href="#" className="btn btn-primary card-btn-style">Github</a>
-                    <a href="#" className="btn btn-primary card-btn-style">Demo</a>
-                </div>
-                </div>
+                </div> */}
             </div>
         </div>
     )
